d6d924e201060a399d098695efee6b53e0378bda
shresthyadavtech <shresthh53@gmail.com>
2026-06-29 11:04:54 +0530

    javascript

---
+++ javascript/conditions.js
@@ -0,0 +1,30 @@
+num = 45;
+if(num %2=== 0){
+    console.log('even');
+
+     let msg = 'hello';
+    // console.log(msg);
+
+    const pi = 3.14;
+     pi = 3.98887;  
+}
+else {
+    console.log('odd');
+
+    
+    
+    
+}
+const age= 20;
+ if(age>= 18){
+    console.log("eligible for dl");
+
+}
+else {
+
+    console.log('not eligible')
+
+        
+}
+
+
---
+++ javascript/index.js
@@ -0,0 +1,15 @@
+console.log('hello javascript')
+
+num = 45;
+
+console.log(num);
+console.log(typeof(num))
+
+myname= "shresth";
+ 
+console.log(typeof(myname))
+
+ishungry=false;
+
+console.log(typeof(ishungry));
+
