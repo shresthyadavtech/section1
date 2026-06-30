fb863448a40b097ae179e45d0258399a3cc3dd9f
shresthyadavtech <shresthh53@gmail.com>
2026-06-30 10:56:14 +0530

    loops js

---
+++ javascript/array.js
@@ -0,0 +1,29 @@
+const arr= [ 'hello','27','false','null']
+    console.log(Array.isArray(arr));
+
+    const movies= ['tere naam' ,'don','biwi no1','dhurandar' ];
+      console.log(movies[3]);
+      console.log(movies.indexOf['don']);
+      
+      console.log(movies.at[-2])
+        console.log(movies);
+
+        console.log(movies.slice(1,4));
+
+        movies.push('junglle')
+        console.log(movies)
+
+        movies.unshift('flash')
+        console.log(movies)
+
+        movies.pop();
+        movies.shift();
+
+        console.log(movies);
+      //  movies.splice(2,2)//removing elements
+      //  movies.splice(2,2,'superman','aquaman')//replace
+        movies.splice(2,0,'superman') //adding element 
+        console.log(movies);
+        
+        
+      
---
+++ javascript/loops.js
@@ -0,0 +1,17 @@
+for (let i = 10; i <= 60; i++) {
+    // Check if the number has a remainder when divided by 2
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
+ let a = 10; 
+ while ( a < 20 ){
+    console.log(a);
+    a++; 
+ };
+ let b=20;
+  do {
+     console.log(b);
+     b++;
+  }
+  while (b<10)
